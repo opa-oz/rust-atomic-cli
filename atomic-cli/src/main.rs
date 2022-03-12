b33c52cf8cda19f900f5fc825a88348bff9a3689
Vladimir Levin <vladimirlevinozinki@gmail.com>
2022-03-12 15:20:53 +0300

    json-generator: ints, objects, strs

--- atomic-cli/src/main.rs
+++ atomic-cli/src/main.rs
@@ -1,7 +1,9 @@
-use json_generator::create_objects_array;
+use json_generator::{create_ints_array, create_objects_array, create_strings_array};
 
 fn main() {
     println!("Hello, world!");
 
-    create_objects_array(10, "/Users/opa_oz/Desktop/result.json", true);
+    create_objects_array(10, "/Users/opa_oz/Desktop/result-objects.json", true);
+    create_strings_array(10, (5, 10), "/Users/opa_oz/Desktop/result-strings.json");
+    create_ints_array(10, (5, 10), "/Users/opa_oz/Desktop/result-ints.json");
 }
--- json-generator/src/lib.rs
+++ json-generator/src/lib.rs
@@ -1,20 +1,25 @@
 use std::fs::{File};
+use std::io::Write;
 
 use json_gen::generate;
 use json_gen::json_template::JsonTemplate;
 
+use rand::Rng;
+
+use lipsum::lipsum;
+
 use serde_json::{to_writer, to_writer_pretty};
 
-const OBJECTS_ARRAY: &str = "
+const OBJECTS_ARRAY: &str = r#"
 {
-    \"|id\": \"seq()\",
-    \"|before\": \"str(5)\",
-    \"|after\": \"str(10)\",
-    \"|date\": \"dt()\",
-    \"|score\": \"int()\",
-    \"|exists\": \"bool()\"
+    "|id": "seq()",
+    "|before": "str(5)",
+    "|after": "str(10)",
+    "|date": "dt()",
+    "|score": "int()",
+    "|exists": "bool()"
 }
-";
+"#;
 
 pub fn create_objects_array(length: usize, output_file: &str, pretty: bool) {
     let mut json_template = JsonTemplate::from_str(OBJECTS_ARRAY, "|").unwrap();
@@ -38,4 +43,51 @@ pub fn create_objects_array(length: usize, output_file: &str, pretty: bool) {
             Ok(_) => println!("successfully wrote to {}", output_file),
         }
     }
+}
+
+pub fn create_strings_array(length: usize, bounds: (usize, usize), output_file: &str) {
+    let mut lines = Vec::new();
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..length {
+        lines.push(
+            format!(
+                "\"{}\"",
+                lipsum(
+                    rng.gen_range(bounds.0..bounds.1)
+                )
+            )
+        );
+    }
+
+    let result = format!("[{}]", lines.join(","));
+
+    write_string_to_file(output_file, &result)
+}
+
+pub fn create_ints_array(length: usize, bounds: (usize, usize), output_file: &str) {
+    let mut ints = Vec::new();
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..length {
+        ints.push(
+            rng.gen_range(bounds.0..bounds.1).to_string()
+        );
+    }
+
+    let result = format!("[{}]", ints.join(","));
+
+    write_string_to_file(output_file, &result)
+}
+
+fn write_string_to_file(output_file: &str, output: &str) {
+    let mut file = match File::create(output_file) {
+        Err(why) => panic!("couldn't create {}: {}", output_file, why),
+        Ok(file) => file,
+    };
+
+    match file.write_all(output.as_ref()) {
+        Err(why) => panic!("couldn't write to {}: {}", output_file, why),
+        Ok(_) => println!("successfully wrote to {}", output_file),
+    };
 }
